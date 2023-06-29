0512185f26c05afc38e5119aa30727dd67b6c27b
Shivam Singh <100526291+shivamsan@users.noreply.github.com>
2023-06-30 00:26:42 +0530

    Add files via upload

---
+++ printfun.py
@@ -0,0 +1,30 @@
+"""The included code stub will read an integer,n , from STDIN.
+
+Without using any string methods, try to print the following:
+
+
+Note that "" represents the consecutive values in between.
+
+Example
+
+Print the string .
+
+Input Format
+
+The first line contains an integer .
+
+Constraints
+
+
+Output Format
+
+Print the list of integers from  through  as a string, without spaces."""
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+if n>=1 or n<=150:
+    
+    for i in range(1,n+1):
+        print(i, end="")
